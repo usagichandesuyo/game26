8cb035a10b50d49e2fe742925ac44cf77c177cc0
うさぎ <purpleeiwa17158@gmail.com>
2026-05-08 15:35:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+const words = [
+  "りんご",
+  "みかん",
+  "さくら",
+  "しんかんせん",
+  "ゲーム",
+  "パソコン",
+  "すいか",
+  "とうきょう",
+  "きょうと",
+  "ねこ"
+];
+
+const train = document.getElementById("train");
+const answerInput = document.getElementById("answer");
+const submitBtn = document.getElementById("submitBtn");
+
+let currentWord = "";
+let position = -300;
+let speed = 6;
+let moveInterval;
+
+let life = 3;
+let combo = 0;
+let score = 0;
+
+function updateUI() {
+  document.getElementById("life").textContent = life;
+  document.getElementById("combo").textContent = combo;
+  document.getElementById("score").textContent = score;
+}
+
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function startRound() {
+
+  clearInterval(moveInterval);
+
+  currentWord = getRandomWord();
+  train.textContent = currentWord;
+
+  position = -300;
+  train.style.left = position + "px";
+
+  moveInterval = setInterval(() => {
+
+    position += speed;
+    train.style.left = position + "px";
+
+    if (position > window.innerWidth) {
+      clearInterval(moveInterval);
+      miss();
+    }
+
+  }, 20);
+}
+
+
+function correct() {
+
+  clearInterval(moveInterval);
+
+  score++;
+  combo++;
+
+  if (combo >= 10) {
+    life++;
+    combo = 0;
+    alert("10コンボ達成！ ライフ回復！");
+  }
+
+  if (score >= 20) {
+    alert("🎉 ゲームクリア！");
+    return;
+  }
+
+  updateUI();
+  startRound();
+}
+
+function miss() {
+
+  combo = 0;
+  life--;
+
+  updateUI();
+
+  if (life <= 0) {
+    alert("💀 ゲームオーバー");
+    clearInterval(moveInterval);
+    return;
+  }
+
+  startRound();
+}
+
+function checkAnswer() {
+
+  const userAnswer = answerInput.value.trim();
+
+  if (userAnswer === currentWord) {
+    correct();
+  } else {
+    miss();
+  }
+
+  answerInput.value = "";
+}
+
+submitBtn.addEventListener("click", checkAnswer);
+
+answerInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    checkAnswer();
+  }
+});
+
+updateUI();
+startRound();
